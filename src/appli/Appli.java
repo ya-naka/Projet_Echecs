7432aca7770eb809d40efe875fd3ccee7d0c6532
yanaka <y.naitkaci@gmail.com>
2021-05-27 21:08:36 +0200

    Second commit

---
+++ src/appli/Appli.java
@@ -0,0 +1,14 @@
+package appli;
+
+public class Appli {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sélectionnez le mode de jeu : ");
+		System.out.println("1 - Joueur VS Joueur\n"
+				+ "2 - Joueur VS Ordinateur\n"
+				+ "3 - Ordinateur VS Ordinateur");
+		Scanner sin = new Scanner()
+	}
+
+}
---
+++ src/joueur/IJoueur.java
@@ -0,0 +1,5 @@
+package joueur;
+
+public interface IJoueur {
+
+}
